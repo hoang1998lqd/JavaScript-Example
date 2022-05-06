8cc528e21b99c683b74fc49c0887d78c2722867a
V Regalite <hoang1998lqd@gmail.com>
2022-05-06 13:39:12 +0700

    Add file thực hành

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function showMessage(){
+    // document.getElementById('exampleA').innerHTML = '<b> Xin chào!</b>';
+    // document.write('Xin chào các bạn')
+    // console.log('Xin chào')
+    alert('Xin chào các bạn')
+}
